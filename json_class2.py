469fe0e0449b260c15ce60b4b3b12cd155477823
brunothetav <brunoccalmeida@hotmail.com>
2021-12-13 17:50:13 -0600

    Json classes done

---
+++ json_class2.py
@@ -0,0 +1,28 @@
+import json
+
+data = '''
+[
+
+  { 
+  "id": "001",
+  "x": "2",
+  "name": "Chuck"
+  },
+
+  { 
+  "id": "009",
+  "x":  "7",
+  "name": "Bruno"
+  }
+
+]
+'''
+
+info = json.loads(data)
+print('User count:', len(info))
+for item in info:
+    print()
+    print('Name', item['name'])
+    print('Id', item['id'])
+    print('Attribute', item['x'])
+    print()
